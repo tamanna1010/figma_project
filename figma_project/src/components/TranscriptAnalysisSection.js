24502d575ba26fecdf8637d8296f2ac2f7838a42
tamanna1010 <98610986+tamanna1010@users.noreply.github.com>
2024-03-12 19:05:03 +0000

    ADD : added a constant

--- figma_project/src/components/TranscriptAnalysisSection.js
+++ figma_project/src/components/TranscriptAnalysisSection.js
@@ -4,50 +4,7 @@ import Timeline from "./Timeline";
 import TonesAnalysis from "./TonesAnalysis";
 import TimelineCard from "./TimelineCard";
 import BarChartComponent from "./Chart";
-const data = [
-  {
-    name: "6",
-    Identification: 35,
-    value: 35,
-    description: "Call Protocol and Identification",
-    colour: "#7367F0",
-  },
-  {
-    name: "5",
-    Information: 20,
-    value: 20,
-    description: "Information and Agreement",
-    colour: "#00CFE8",
-  },
-  {
-    name: "4",
-    Interaction: 14,
-    value: 14,
-    description: "Customer Interaction and Verification",
-    colour: "#28C76F",
-  },
-  {
-    name: "3",
-    Relationship: 12,
-    value: 12,
-    description: "Customer Relationship Management & Feedback",
-    colour: "#A8AAAE",
-  },
-  {
-    name: "2",
-    Communication: 10,
-    value: 10,
-    description: "Communication Style",
-    colour: "#EA5455",
-  },
-  {
-    name: "1",
-    Professionalism: 9,
-    value: 9,
-    description: "Call Closure & Professionalism",
-    colour: "#FF9F43",
-  },
-];
+import { data } from "../utils/data";
 const TranscriptAnalysisSection = (props) => {
   const { title, timelineData } = props;
   return (
---
+++ figma_project/src/utils/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    name: "6",
+    Identification: 35,
+    value: 35,
+    description: "Call Protocol and Identification",
+    colour: "#7367F0",
+  },
+  {
+    name: "5",
+    Information: 20,
+    value: 20,
+    description: "Information and Agreement",
+    colour: "#00CFE8",
+  },
+  {
+    name: "4",
+    Interaction: 14,
+    value: 14,
+    description: "Customer Interaction and Verification",
+    colour: "#28C76F",
+  },
+  {
+    name: "3",
+    Relationship: 12,
+    value: 12,
+    description: "Customer Relationship Management & Feedback",
+    colour: "#A8AAAE",
+  },
+  {
+    name: "2",
+    Communication: 10,
+    value: 10,
+    description: "Communication Style",
+    colour: "#EA5455",
+  },
+  {
+    name: "1",
+    Professionalism: 9,
+    value: 9,
+    description: "Call Closure & Professionalism",
+    colour: "#FF9F43",
+  },
+];
